7d408421b4a7f27c1c802293e38fc9fbdf93317d
baudointran <baudointran@users.noreply.github.com>
2022-08-22 15:36:08 +0200

    init

---
+++ app/src/main/java/com/example/myfirstapp/FirstFragment.java
@@ -0,0 +1,58 @@
+package com.example.myfirstapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.widget.Toast;
+
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.myfirstapp.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+
+        view.findViewById(R.id.toast_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast myToast = Toast.makeText(getActivity(), R.string.message_toast, Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
